2e86a776cd25ede86ca60132319843928c8daca1
Natalia Ortiz Vasquez <nortiz@lenovo9669.bogota.agofer>
2026-01-22 11:00:44 -0500

    a new application

---
+++ estate/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Natalia Ortiz",
+    'category': 'apps',
+    'description': """
+    learning about odoo
+    """,
+    "application": True,
+
+    # data files always loaded at installation
+    'data': [
+        'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+}
